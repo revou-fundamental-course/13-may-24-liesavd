a9bcbb3ac68cf3fe8d546df2e452fc43f34332f2
liesavd <liesa.dayanti@pjnhk.go.id>
2024-05-20 21:14:49 +0700

    tambah keliling segitiga

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const alasInput = document.getElementById('alas');
 const tinggiInput = document.getElementById('tinggi');
+const aInput = document.getElementById('a');
+const bInput = document.getElementById('b');
+const cInput = document.getElementById('c');
 const hitungButton = document.getElementById('hitung');
+const hitungkelButton = document.getElementById('hitungkel');
 const resultElement = document.getElementById('result');
+const hasiltElement = document.getElementById('hasil');
 const resetButton = document.getElementById('reset');
 
 
@@ -13,9 +18,26 @@ hitungButton.addEventListener('click', (e) => {
     resultElement.textContent = `L = ${luas}`;
 });
 
+hitungkelButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const a = parseInt(aInput.value);
+    const b = parseInt(bInput.value);
+    const c = parseInt(cInput.value);
+    const keliling = a + b + c;
+    hasiltElement.textContent = `Keliling Segitiga = ${keliling}`;
+});
+
 resetButton.addEventListener('click', () => {
     // Reset all input fields and results
     document.getElementById('alas').value = '';
     document.getElementById('tinggi').value = '';
     document.getElementById('result').textContent = '';
+  });
+
+  resetButton.addEventListener('click', () => {
+    // Reset all input fields and hasil
+    document.getElementById('a').value = '';
+    document.getElementById('b').value = '';
+    document.getElementById('c').value = '';
+    document.getElementById('hasil').textContent = '';
   });

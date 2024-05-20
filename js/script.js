966228d86753aafbac864ffc4ec9befc94488a61
liesavd <liesa.dayanti@pjnhk.go.id>
2024-05-20 18:29:54 +0700

    Tambah navbar dan button reset

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const alasInput = document.getElementById('alas');
 const tinggiInput = document.getElementById('tinggi');
 const hitungButton = document.getElementById('hitung');
 const resultElement = document.getElementById('result');
+const resetButton = document.getElementById('reset');
+
 
 hitungButton.addEventListener('click', (e) => {
     e.preventDefault();
@@ -10,3 +12,10 @@ hitungButton.addEventListener('click', (e) => {
     const luas = (alas * tinggi) / 2;
     resultElement.textContent = `L = ${luas}`;
 });
+
+resetButton.addEventListener('click', () => {
+    // Reset all input fields and results
+    document.getElementById('alas').value = '';
+    document.getElementById('tinggi').value = '';
+    document.getElementById('result').textContent = '';
+  });
